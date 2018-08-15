aac2020dd44855f64c5977976889258756c3e311
Dominique Rochefort <drochefort@tribalnova.com>
2018-08-15 13:15:17 -0400

    component base yeah

--- packages/component-base/src/components/component-base.ts
+++ packages/component-base/src/components/component-base.ts
@@ -1,10 +1,5 @@
-import { html, LitElement } from '@polymer/lit-element/lit-element';
-import { TemplateResult } from 'lit-html/lit-html';
+import { LitElement } from '@polymer/lit-element/lit-element';
 
 export class ComponentBase extends LitElement {
-    protected _render(props: LitElement): TemplateResult {
-        return html``;
-    }
 }
 
-customElements.define('component-base', ComponentBase);
