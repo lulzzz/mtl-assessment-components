c5d83dd62415c036b7af3400a3261f4c4a1a56fc
Christopher Morris <cmorris@tribalnova.com>
2018-08-16 13:19:11 -0400

    update test

--- packages/drop-down/src/unit/drop-down.spec.ts
+++ packages/drop-down/src/unit/drop-down.spec.ts
@@ -1,15 +1,9 @@
 const expect: any = chai.expect;
-const tagName: string = 'text-input';
+const tagName: string = 'drop-down';
 
 describe(`<${tagName}>`, (): void => {
     it('should render as expected', (): void => {
         withSnippet('default');
-        const value: boolean = true;
-        expect(value).to.be.true;
-    });
-
-    it('should render within a paragraph', (): void => {
-        withSnippet('paragraph');
         const value: boolean = true;
         expect(value).to.be.true;
     });
