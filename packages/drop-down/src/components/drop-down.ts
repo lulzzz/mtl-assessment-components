2d6b43e3f8b7f8b58202743e1ec9e1cbd026592f
Christopher Morris <cmorris@tribalnova.com>
2018-08-17 11:59:19 -0400

    slots not quite working

--- packages/drop-down/src/components/drop-down.ts
+++ packages/drop-down/src/components/drop-down.ts
@@ -1,10 +1,9 @@
 import { ComponentBase, html, TemplateResult } from '@hmh/component-base/dist/components/component-base';
-import { FeedbackMixin } from '@hmh/component-base/dist/components/feedback-mixin';
 import { PersistenceMixin } from '@hmh/component-base/dist/components/persistence-mixin';
 // @ts-ignore : no type definition available
 import { MDCSelect } from '@material/select/index.js';
 
-export class DropDown extends ComponentBase implements FeedbackMixin, PersistenceMixin {
+export class DropDown extends ComponentBase implements PersistenceMixin {
     public values: string = '';
 
     static get properties(): { [key: string]: string | object } {
@@ -25,24 +24,12 @@ export class DropDown extends ComponentBase implements FeedbackMixin, Persistenc
     }
 
     protected _render({ values }: DropDown): TemplateResult {
-
-        const valuesArr = values.split(',');
-        //TODO: loop
         return html`
         <link rel="stylesheet" type="text/css" href="/node_modules/@material/select/dist/mdc.select.css">
         <div class="mdc-select">
-        <select class="mdc-select__native-control">
-          <option value="" disabled selected></option>
-          <option value=${valuesArr[0]}>
-            ${valuesArr[1]}
-          </option>
-          <option value=${valuesArr[2]}>
-            ${valuesArr[3]}
-          </option>
-          <option value=${valuesArr[4]}>
-            ${valuesArr[5]}
-          </option>
-        </select>
+            <select class="mdc-select__native-control">
+                <slot name="options"></slot>
+            </select>
         <label class="mdc-floating-label">Pick a Food Group</label>
         <div class="mdc-line-ripple"></div>
       </div>
