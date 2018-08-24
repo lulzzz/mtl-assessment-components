7fbc7b31f53f29779a3a598f9138e1129edecbe3
Christopher Morris <cmorris@tribalnova.com>
2018-08-24 12:29:45 -0400

    accessablity

--- packages/drop-down-two/src/components/drop-down.ts
+++ packages/drop-down-two/src/components/drop-down.ts
@@ -1,7 +1,6 @@
 import { ComponentBase, html, TemplateResult } from '@hmh/component-base/dist/index';
 import { Persistence } from '@hmh/component-base/dist/mixins/persistence';
 // @ts-ignore : no type definition available
-// import { MDCSelect } from '@material/select/index.js';
 
 export class DropDown extends ComponentBase implements Persistence {
     public values: string = '';
@@ -12,19 +11,26 @@ export class DropDown extends ComponentBase implements Persistence {
         };
     }
 
-    public onDropDownClicked(): void {
+    private onDropDownClicked(): void {
         const dropDown = this.shadowRoot.getElementById('myDropdown')
         dropDown.classList.toggle("show");
         dropDown.focus();
     }
 
-    public onItemClicked(event: MouseEvent, eventTarget: HTMLElement): void {
+    private onItemClicked(event: MouseEvent, eventTarget: HTMLElement): void {
         if (eventTarget.hasAttribute('slot')) {
+            this.values = eventTarget.getAttribute('value');
+            eventTarget.setAttribute('aria-selected', 'true');
+            console.log('this.values:', this.values);
             this.shadowRoot.querySelector('.dropbtn').innerHTML = eventTarget.innerHTML;
             this.onDropDownClicked();
         } else {
             this.onItemClicked(event, eventTarget.parentNode as HTMLElement)
         }
+    }
+
+    protected _didRender(): void {
+        this.enableAccessibility();
     }
 
     protected _render({ values }: DropDown): TemplateResult {
@@ -40,10 +46,16 @@ export class DropDown extends ComponentBase implements Persistence {
                 </div>
             </div>
             <div id="myDropdown" class="dropdown-content">
-                <slot name="options" class="options" on-click="${(evt: MouseEvent) => this.onItemClicked(evt, evt.target as HTMLElement)}"> </slot>
+                <slot name="options" role="option" class="options" on-click="${(evt: MouseEvent) => this.onItemClicked(evt, evt.target as HTMLElement)}"> </slot>
             </div>
         </div>
         `;
+    }
+
+    private enableAccessibility(): void {
+        this.setAttribute('role', 'popupbutton');
+        this.setAttribute('aria-haspopup', 'true');
+        this.setAttribute('aria-label', this.shadowRoot.querySelector('.dropbtn').innerHTML);
     }
 }
 
