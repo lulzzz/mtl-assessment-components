baa854bd939607763baf00d62041e8250822619e
chrisunl <christopher.morris@hmhco.com>
2018-09-06 16:02:12 -0400

    Fix response validation (#26)
    * delete response validation package
    
    * fixes
    
    * fixes

--- packages/component-base/src/mixins/feedback.ts
+++ packages/component-base/src/mixins/feedback.ts
@@ -1,18 +1,17 @@
-import { FeedbackMessage, ResponseValidation } from '@hmh/response-validation/dist/components/response-validation';
-
+import {
+    FeedbackMessage,
+    ResponseValidation
+} from '../components/response-validation';
 /**
  * All components that have feedback must implement this mixin
  */
-
 export enum Strategy {
     EXACT_MATCH = 'exactMatch',
-    FUZZY_MATCH = 'fuzzyMatch',
-    MATH_EQUIVALENT = 'mathEquivalent'
+        FUZZY_MATCH = 'fuzzyMatch',
+        MATH_EQUIVALENT = 'mathEquivalent'
 }
-
 export abstract class Feedback {
     public abstract _responseValidationElements: ResponseValidation[];
-
     public _getFeedback(value: any): FeedbackMessage {
         for (const el of this._responseValidationElements) {
             if (this.match(el, value)) {
@@ -22,6 +21,7 @@ export abstract class Feedback {
         throw new Error('missing default response-validation');
     }
     public match(el: ResponseValidation, response: any): boolean {
+        console.log('MATCH', el, el.getExpected);
         if (!el.getExpected()) {
             // catch-all clause
             return true;
@@ -41,6 +41,19 @@ export abstract class Feedback {
                 return equals;
             default:
                 return false;
+        }
+    }
+    public _onFeedbackSlotChanged(evt: Event): void {
+        const slot: HTMLSlotElement = evt.srcElement as HTMLSlotElement;
+        if (slot) {
+            const nodes: ResponseValidation[] = slot.assignedNodes() as any[];
+            if (nodes) {
+                const responseValidationElements: ResponseValidation[] = [];
+                for (const el of nodes as ResponseValidation[]) {
+                    responseValidationElements.push(el);
+                }
+                this._responseValidationElements = responseValidationElements;
+            }
         }
     }
 }
