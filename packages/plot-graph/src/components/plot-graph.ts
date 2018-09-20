8bf9dd5b50c88d9ab6f9d6af7bb8b1c29305f4b8
Christopher Morris <cmorris@tribalnova.com>
2018-09-20 14:34:54 -0400

    fix text entry

--- packages/plot-graph/src/components/plot-graph.ts
+++ packages/plot-graph/src/components/plot-graph.ts
@@ -7,8 +7,9 @@ import { MDCTextField } from '@material/textfield/index';
  */
 export class PlotGraph extends ComponentBase<string>{
     public value: string;
-    public disableInput: boolean = false;
-    private placeholderText: string = 'Enter Equation Here';
+    // public disableInput: boolean = false;
+    private placeholderText: string = 'Solve for Y:';
+    private textFieldDisabled: boolean = false;
 
 
     static get properties(): { [key: string]: string | object } {
@@ -18,14 +19,15 @@ export class PlotGraph extends ComponentBase<string>{
             y1: String,
             x2: String,
             y2: String,
-            disableInput: Boolean
+            disableInput: Boolean,
+            placeholderText: String,
+            textFieldDisabled: Boolean
         };
     }
     
     private _onTextInputChange(evt: Event): void {
         evt.stopPropagation();
         const text = (evt.target as HTMLInputElement).value;
-        console.log('text:', text);
         this.parseEquation(text);
     }
 
@@ -34,32 +36,38 @@ export class PlotGraph extends ComponentBase<string>{
         this.plotGraph(0,0,50,50);
     }
 
-    protected _render({disableInput}: PlotGraph): TemplateResult {
+    protected _render({ textFieldDisabled, placeholderText }: PlotGraph): TemplateResult {
         return html`
+        <link rel="stylesheet" type="text/css" href="/node_modules/@material/textfield/dist/mdc.textfield.css">
         <link rel="stylesheet" type="text/css" href="/dist/css/plot-graph.css">
-        <canvas id="canvas" width="100" height="100"></canvas>
-        <div class$="mdc-text-field mdc-text-field--outlined">
-            <input
-                disabled$="${disableInput}"
-                placeholder="${this.placeholderText}" 
-                type="text" 
-                id="tf-outlined" 
-                class="mdc-text-field__input" 
-                value="" 
-                on-change="${(evt: Event) => this._onTextInputChange(evt)}" />
-            <div class="mdc-notched-outline">
-                <svg>
-                <path class="mdc-notched-outline__path"/>
-                </svg>
+        <div class="container">
+            
+                <canvas id="canvas" width="100" height="100"></canvas>
+            
+            <div class$="mdc-text-field mdc-text-field--outlined ${textFieldDisabled ? 'mdc-text-field--disabled' : ''}">
+                <input
+                    disabled="${textFieldDisabled}"
+                    type="text" 
+                    id="tf-outlined" 
+                    class="mdc-text-field__input" 
+                    value="" 
+                    placeholder="${placeholderText}" 
+                    on-change="${(evt: Event) => this._onTextInputChange(evt)}" />
+                <div class="mdc-notched-outline">
+                    <svg>
+                    <path class="mdc-notched-outline__path"/>
+                    </svg>
+                </div>
+                <div class="mdc-notched-outline__idle"></div>
             </div>
-            <div class="mdc-notched-outline__idle"></div>
-        </div>
+         </div>
         `;
     }
-    
-    protected _didRender() {
+
+    protected _didRender(): void {
+        const elem = this.shadowRoot.querySelector('.mdc-text-field');          
+        MDCTextField.attachTo(elem);
         this.plotGraph(parseInt(this.x1),parseInt(this.y1),parseInt(this.x2), parseInt(this.y2));
-        MDCTextField.attachTo(this.shadowRoot.querySelector('.mdc-text-field'));
     }
 
     private plotGraph(x1: number, y1: number, x2: number, y2: number ) {
@@ -71,7 +79,7 @@ export class PlotGraph extends ComponentBase<string>{
         ctx.stroke();
         if (JSON.stringify(this.value) != JSON.stringify([x1,y1,x2,y2])) {
             this.value = JSON.stringify([x1,y1,x2,y2]);
-
+            
             this.dispatchEvent(
                 new CustomEvent('change', {
                     bubbles: true,
