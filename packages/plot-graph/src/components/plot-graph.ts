5e37fd1ddbe163da547ed323c9d931c5f2391ddc
Christopher Morris <cmorris@tribalnova.com>
2018-09-25 16:03:22 -0400

    added feedback

--- packages/plot-graph/src/components/plot-graph.ts
+++ packages/plot-graph/src/components/plot-graph.ts
@@ -1,5 +1,6 @@
-import { ComponentBase, html, TemplateResult } from "@hmh/component-base/dist/index";
+import { ComponentBase, html, TemplateResult, Feedback, FeedbackMessage, Strategy, applyMixins } from "@hmh/component-base/dist/index";
 import { MDCTextField } from "@material/textfield/index";
+import { ResponseValidation } from '@hmh/component-base/dist/index';
 import * as d3 from "d3";
 
 class Line {
@@ -28,100 +29,153 @@ class Line {
         this._startY = y1;
         this._endY = y2;
     }
+
+    toString(): string {
+        return 'X: ' + this.startX.toString() + ' to ' + this.endX.toString() + 
+        ' Y: ' + this.startY.toString() + ' to ' + this.endY.toString();
+    }
 }
 
 /**
  * `<plot-graph>`
  * @demo ./demo/index.html
  */
-export class PlotGraph extends ComponentBase<string> {
-  private placeholderText: string = "Solve for Y:";
-  private textFieldDisabled: boolean = false;
-  private xValues: string;
-  private equation: string;
+export class PlotGraph extends ComponentBase<string> implements Feedback {
+    private placeholderText: string = "Solve for Y:";
+    private textFieldDisabled: boolean = false;
+    private xValues: string;
+    private equation: string;
 
-  private yMin: number = 0;
-  private yMax: number = 50;
+    private yMin: number = 0;
+    private yMax: number = 100;
+    private lineDescription: string = '';
 
-  static get properties(): { [key: string]: string | object } {
-    return {
-        ...super.properties,
-        disableInput: Boolean,
-        placeholderText: String,
-        textFieldDisabled: Boolean,
-        xValues: String,
-        equation: String,
-    };
-  }
+    static get properties(): { [key: string]: string | object } {
+        return {
+            ...super.properties,
+            disableInput: Boolean,
+            placeholderText: String,
+            textFieldDisabled: Boolean,
+            xValues: String,
+            equation: String,
+            lineDescription: String
+        };
+    }
 
-  protected _render({ textFieldDisabled, placeholderText }: PlotGraph): TemplateResult {
-    return html`
-    <link rel="stylesheet" type="text/css" href="/node_modules/@material/textfield/dist/mdc.textfield.css">
-    <link rel="stylesheet" type="text/css" href="/dist/css/plot-graph.css">
-    <div class="container">
-        <div id='canvas'></div>
-        <div class$="mdc-text-field mdc-text-field--outlined ${ textFieldDisabled ? "mdc-text-field--disabled" : "" }">
-            <input
-                disabled="${textFieldDisabled}"ß
-                type="text"
-                id="tf-outlined"
-                class="mdc-text-field__input"
-                value=""
-                placeholder="${placeholderText}"
-                on-change="${(evt: Event) =>
-                this._onTextInputChange(evt)}"/>
-            <div class="mdc-notched-outline">
-                <svg> <path class="mdc-notched-outline__path"/></svg>
-            </div>
-            <div class="mdc-notched-outline__idle"></div>
-        </div>
-    </div>
-    `;
-  }
+    // @mixin: feedback
+    public computeFeedback: (value: string) => FeedbackMessage;
+    public _onFeedbackSlotChanged: any;
 
-  public _firstRendered(): void {
-    const elem = this.shadowRoot.querySelector(".mdc-text-field");
-    MDCTextField.attachTo(elem);
-    // get an array of numbers from the string input
-    const xValues: number[] = this.xValues.split(",").map(Number);
-    const lines: Line[] = [];
-
-    xValues.forEach((xValue, index) => {
-        if ((index + 1) % 2 == 0) {
-            // max and min X for each line, Get max min Y per this.equation
-            lines.push(this.applyEquation(this.equation, xValues[index - 1], xValue));
+    public match(el: ResponseValidation, response: string): boolean {
+        if (!el.expected) {
+            // catch-all clause
+            return true;
         }
-    });
 
-    // use D3 for SVG
-    let svgContainer = d3.select(this.shadowRoot).select("#canvas").append("svg").attr("width", 100).attr("height", 100);
+        switch (el.strategy) {
+            case Strategy.FUZZY_MATCH:
+                return el.expected.toLowerCase() === (response ? response.toLowerCase() : '');
+            case Strategy.EXACT_MATCH:
+            default:
+                return el.expected === (response ? response.toLowerCase() : '');
+        }
+    }
 
-    // draw the lines
-    lines.forEach((line) => {
-        svgContainer.append("line")
-        .attr("x1", line.startX)
-        .attr("y1", line.startY)
-        .attr("x2", line.endX)
-        .attr("y2", line.endY)
-        .attr("stroke-width", 1)
-        .attr("stroke", 'red');
-    });
-  }
+    protected _render({ textFieldDisabled, placeholderText, lineDescription, feedbackMessage }: PlotGraph): TemplateResult {
+        console.log('feedbackMessage: ', feedbackMessage);
 
-  // TODO : this is meant to be the answer
-  private _onTextInputChange(evt: Event): void {
-    evt.stopPropagation();
-    // const text = (evt.target as HTMLInputElement).value;
-  }
+        return html`
+        <link rel="stylesheet" type="text/css" href="/node_modules/@material/textfield/dist/mdc.textfield.css">
+        <link rel="stylesheet" type="text/css" href="/dist/css/plot-graph.css">
 
-  // This is a mock
-  private applyEquation(equation: string, xMin: number, xMax: number): Line {
-    console.log("equation: ", equation);
-    let line = new Line(xMin, xMax, this.yMin, this.yMax);
-    this.yMin += 50;
-    this.yMax += 50;
-    return line;
-  }
+        <div class="container" class$="${this._getFeedbackClass(feedbackMessage, true)}">
+            <div id='canvas'></div>
+            <div id='line-description'> ${lineDescription ? lineDescription : ''} </div>
+
+            <div class="text-field-container">
+                <div class$="mdc-text-field mdc-text-field--outlined ${ textFieldDisabled ? "mdc-text-field--disabled" : "" }">
+                    <input
+                        disabled="${textFieldDisabled}"ß
+                        type="text"
+                        id="tf-outlined"
+                        class="mdc-text-field__input"
+                        value=""
+                        placeholder="${placeholderText}"
+                        on-change="${(evt: Event) =>
+                        this._onTextInputChange(evt)}"/>
+                    <div class="mdc-notched-outline">
+                        <svg> <path class="mdc-notched-outline__path"/></svg>
+                    </div>
+                    <div class="mdc-notched-outline__idle"></div>
+                </div>
+                <span class$="feedback-message ${this._getFeedbackClass(feedbackMessage, false)}">${feedbackMessage ? feedbackMessage.message : ''}</span>            
+            </div>
+        </div>
+
+        <slot hidden name="feedback" on-slotchange="${(evt: Event) => this._onFeedbackSlotChanged(evt)}"></slot>
+        `;
+    }
+
+    /**
+     * Get feedback classes for UI depending upon feedbackMessage.type
+     *
+     * @param  {FeedbackMessage} feedbackMessage
+     * @returns String
+     */
+    private _getFeedbackClass(feedbackMessage: FeedbackMessage, isContainer: boolean): string {
+        if (!feedbackMessage) {
+            return '';
+        }
+
+        return isContainer ? `feedback-${feedbackMessage.type}-border` : `feedback-${feedbackMessage.type}-background`;
+    }
+  
+    public _firstRendered(): void {
+        const elem = this.shadowRoot.querySelector(".mdc-text-field");
+        MDCTextField.attachTo(elem);
+        // get an array of numbers from the string input
+        const xValues: number[] = this.xValues.split(",").map(Number);
+        const lines: Line[] = [];
+
+        xValues.forEach((xValue, index) => {
+            if ((index + 1) % 2 == 0) {
+                // max and min X for each line, Get max min Y per this.equation
+                lines.push(this.applyEquation(this.equation, xValues[index - 1], xValue));
+            }
+        });
+
+        // use D3 for SVG
+        let svgContainer = d3.select(this.shadowRoot).select("#canvas").append("svg").attr("width", 100).attr("height", 100);
+
+        // draw the lines
+        lines.forEach((line) => {
+            svgContainer.append("line")
+            .attr("x1", line.startX)
+            .attr("y1", line.startY)
+            .attr("x2", line.endX)
+            .attr("y2", line.endY)
+            .attr("stroke-width", 1)
+            .attr("stroke", 'red');
+            this.lineDescription += line.toString() + ' ';
+        });
+    }
+
+  
+    private _onTextInputChange(evt: Event): void {
+        evt.stopPropagation();
+        this.value = (evt.target as HTMLInputElement).value;
+    }
+
+    // This is a mock
+    private applyEquation(equation: string, xMin: number, xMax: number): Line {
+        console.log("equation: ", equation);
+        let line = new Line(xMin, xMax, this.yMin, this.yMax);
+        this.yMin += 50;
+        this.yMax += 50;
+        return line;
+    }
 }
+
+applyMixins(PlotGraph, [Feedback]);
 
 customElements.define("plot-graph", PlotGraph);
