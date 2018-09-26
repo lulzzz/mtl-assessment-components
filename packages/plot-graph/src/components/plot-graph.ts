88fdb212474cb87fe811d0752d15a3a7489608f8
Christopher Morris <cmorris@tribalnova.com>
2018-09-26 13:56:59 -0400

    remove feedback

--- packages/plot-graph/src/components/plot-graph.ts
+++ packages/plot-graph/src/components/plot-graph.ts
@@ -1,6 +1,4 @@
-import { ComponentBase, html, TemplateResult, Feedback, FeedbackMessage, Strategy, applyMixins } from "@hmh/component-base/dist/index";
-import { MDCTextField } from "@material/textfield/index";
-import { ResponseValidation } from '@hmh/component-base/dist/index';
+import { ComponentBase, html, TemplateResult } from "@hmh/component-base/dist/index";
 import * as d3 from "d3";
 
 class Line {
@@ -40,7 +38,7 @@ class Line {
  * `<plot-graph>`
  * @demo ./demo/index.html
  */
-export class PlotGraph extends ComponentBase<string> implements Feedback {
+export class PlotGraph extends ComponentBase<string> {
     private placeholderText: string = "Solve for Y:";
     private textFieldDisabled: boolean = false;
     private xValues: string;
@@ -63,28 +61,7 @@ export class PlotGraph extends ComponentBase<string> implements Feedback {
         };
     }
 
-    // @mixin: feedback
-    public computeFeedback: (value: string) => FeedbackMessage;
-    public _onFeedbackSlotChanged: any;
-
-    public match(el: ResponseValidation, response: string): boolean {
-        if (!el.expected) {
-            // catch-all clause
-            return true;
-        }
-
-        switch (el.strategy) {
-            case Strategy.FUZZY_MATCH:
-                return el.expected.toLowerCase() === (response ? response.toLowerCase() : '');
-            case Strategy.EXACT_MATCH:
-            default:
-                return el.expected === (response ? response.toLowerCase() : '');
-        }
-    }
-
     public _firstRendered(): void {
-        const elem = this.shadowRoot.querySelector(".mdc-text-field");
-        MDCTextField.attachTo(elem);
         // get an array of numbers from the string input
         const xValues: number[] = this.xValues.split(",").map(Number);
         const lines: Line[] = [];
@@ -125,61 +102,16 @@ export class PlotGraph extends ComponentBase<string> implements Feedback {
         return line;
     }
 
-    protected _render({ textFieldDisabled, placeholderText, lineDescription, feedbackMessage }: PlotGraph): TemplateResult {
-        console.log('feedbackMessage: ', feedbackMessage);
-
+    protected _render({ textFieldDisabled, placeholderText, lineDescription }: PlotGraph): TemplateResult {
         return html`
-        <link rel="stylesheet" type="text/css" href="/node_modules/@material/textfield/dist/mdc.textfield.css">
         <link rel="stylesheet" type="text/css" href="/dist/css/plot-graph.css">
 
-        <div class$="container ${this._getFeedbackClass(feedbackMessage, true)}">
+        <div class="container">
             <div id='canvas'></div>
             <div class='line-description'> ${lineDescription ? lineDescription : ''} </div>
-
-            <div class="bottom-ui-container">
-                <div class$="mdc-text-field mdc-text-field--outlined ${ textFieldDisabled ? "mdc-text-field--disabled" : "" }">
-                    <input
-                        disabled="${textFieldDisabled}"ß
-                        type="text"
-                        id="tf-outlined"
-                        class="mdc-text-field__input"
-                        value=""
-                        placeholder="${placeholderText}"
-                        on-change="${(evt: Event) =>
-                        this._onTextInputChange(evt)}"/>
-                    <div class="mdc-notched-outline">
-                        <svg> <path class="mdc-notched-outline__path"/></svg>
-                    </div>
-                    <div class="mdc-notched-outline__idle"></div>
-                </div>
-                <span class$="feedback-message ${this._getFeedbackClass(feedbackMessage, false)}">${feedbackMessage ? feedbackMessage.message : ''}</span>          
-            </div>
         </div>
-
-        <slot hidden name="feedback" on-slotchange="${(evt: Event) => this._onFeedbackSlotChanged(evt)}"></slot>
         `;
     }
-
-    /**
-     * Get feedback classes for UI depending upon feedbackMessage.type
-     *
-     * @param  {FeedbackMessage} feedbackMessage
-     * @returns String
-     */
-    private _getFeedbackClass(feedbackMessage: FeedbackMessage, isContainer: boolean): string {
-        if (!feedbackMessage) {
-            return '';
-        }
-
-        return isContainer ? `feedback-${feedbackMessage.type}-border` : `feedback-${feedbackMessage.type}-background`;
-    }
-  
-    private _onTextInputChange(evt: Event): void {
-        evt.stopPropagation();
-        this.value = (evt.target as HTMLInputElement).value;
-    }
 }
-
-applyMixins(PlotGraph, [Feedback]);
 
 customElements.define("plot-graph", PlotGraph);
