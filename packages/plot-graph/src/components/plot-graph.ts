f9fad59d924d3e69b73f839808bc7122e53a7fea
Christopher Morris <cmorris@tribalnova.com>
2018-09-26 18:08:17 -0400

    refactor wip

--- packages/plot-graph/src/components/plot-graph.ts
+++ packages/plot-graph/src/components/plot-graph.ts
@@ -1,114 +1,123 @@
 import { ComponentBase, html, TemplateResult } from "@hmh/component-base/dist/index";
 import * as d3 from "d3";
 
-class Line {
-    private static _nullValue: number = -10000;
-    private _startX: number = Line._nullValue;
-    private _endX: number = Line._nullValue;
-    private _startY: number = Line._nullValue;
-    private _endY: number = Line._nullValue;
-
-    get startX(): number {
-        return this._startX;
-    }
-    get endX(): number {
-        return this._endX;
-    }
-    get startY(): number {
-        return this._startY;
-    }
-    get endY(): number {
-        return this._endY;
-    }
-
-    constructor(x1?: number, x2?: number, y1?: number, y2?: number) {
-        this._startX = x1;
-        this._endX = x2;
-        this._startY = y1;
-        this._endY = y2;
-    }
-
-    toString(): string {
-        return 'X: ' + this.startX.toString() + ' to ' + this.endX.toString() + 
-        ' Y: ' + this.startY.toString() + ' to ' + this.endY.toString();
-    }
-}
-
 /**
  * `<plot-graph>`
  * @demo ./demo/index.html
  */
 export class PlotGraph extends ComponentBase<string> {
-    private xValues: string;
-    private equation: string;
 
-    private yMin: number = 0;
     private graphSize: number = 500;
-    private yMax: number = 100; // this.graphSize;
-    private lineDescription: string = '';
-
+    private xmin: number = 0;
+    private xmax: number = 0;
+    private yMin: number = 0;
+    private yMax: number = 0;
+    private step: number = 0;
+    private equations: string[] = [];
+    private svgContainer: any = null;
+    private renderedGraph: boolean = false;
+    
     static get properties(): { [key: string]: string | object } {
         return {
             ...super.properties,
-            disableInput: Boolean,
-            placeholderText: String,
-            textFieldDisabled: Boolean,
-            xValues: String,
-            equation: String,
-            lineDescription: String
+            xmin: Number,
+            xmax: Number,
+            ymin: Number,
+            ymax: Number,
+            step: Number
         };
     }
 
-    public _firstRendered(): void {
-        // get an array of numbers from the string input
-        const xValues: number[] = this.xValues.split(",").map(Number);
-        const lines: Line[] = [];
-
-        xValues.forEach((xValue, index) => {
-            if ((index + 1) % 2 == 0) {
-                // max and min X for each line, Get max min Y per this.equation
-                lines.push(this.applyEquation(this.equation, xValues[index - 1], xValue));
-            }
-        });
-
-        // use D3 for SVG
-        let svgContainer = d3.select(this.shadowRoot).select("#canvas").append("svg").attr("width", this.graphSize).attr("height", this.graphSize);
-
-        // draw the lines
-        lines.forEach((line) => {
-            svgContainer.append("line")
-            .attr("x1", this.scaleValToGraph(line.startX))
-            .attr("y1", this.scaleValToGraph(line.startY))
-            .attr("x2", this.scaleValToGraph(line.endX))
-            .attr("y2", this.scaleValToGraph(line.endY))
-            .attr("stroke-width", 1)
-            .attr("stroke", 'red');
-            this.lineDescription += line.toString() + ' ';
-        });
+    // This is a mock
+    private prepareValue(equation: string, x: number, y?: number) : number {
+        const evalMe = equation.replace('x', x.toString());
+        return eval(evalMe);
     }
 
     private scaleValToGraph(val: number) : number {
         return (this.graphSize / 100) * val;
     }
 
-    // This is a mock
-    private applyEquation(equation: string, xMin: number, xMax: number): Line {
-        console.log("equation: ", equation);
-        let line = new Line(xMin, xMax, this.yMin, this.yMax);
-        this.yMin += this.graphSize;
-        this.yMax += this.graphSize;
-        return line;
+//This is the accessor function we talked about above
+    private getPoints(equation: string): string {
+        let results: string = '';
+
+        for (let x = this.xmin; x <= this.xmax; x+=this.step) {
+            const y = this.prepareValue(equation, x);
+            // results.push({"x": x, "y": y});
+            // console.log('x: ', x, ' y: ', y);
+            results+=`${this.scaleValToGraph(x).toString()},${this.scaleValToGraph(y).toString()}`;
+        }
+
+        return results;
     }
 
-    protected _render({ lineDescription }: PlotGraph): TemplateResult {
+    public _didRender(): void {
+        console.log('_didRender:', Date.now());
+        
+        if (!this.renderedGraph && this.equations.length > 0) { //hacky badness
+            /*
+            let lineFunction = d3.svg.line()
+            .x(function(d: any) { return d.x; })
+            .y(function(d: any) { return d.y; })
+            .interpolate("linear");*/
+
+            this.renderedGraph = true;
+            this.svgContainer = d3.select(this.shadowRoot).select("#canvas").append("svg").attr("width", this.graphSize).attr("height", this.graphSize);
+
+            this.equations.forEach((equation) => {
+                const points = this.getPoints(equation);
+                /*
+                this.svgContainer.append("path")
+                                            .attr("d", lineFunction(points))
+                                            .attr("stroke", "blue")
+                                            .attr("stroke-width", 2)
+                                            .attr("fill", "none");
+
+                */
+                console.log('points: ', points);
+                this.svgContainer.append("polyline")
+                .attr("points", points)
+                .attr("stroke-width", 1)
+                .attr("stroke", 'red');                
+            });
+        }
+    }
+
+    protected _render({equations}: PlotGraph): TemplateResult {
+        console.log('render', Date.now());
         return html`
         <link rel="stylesheet" type="text/css" href="/dist/css/plot-graph.css">
 
         <div class="container">
-            <div id='canvas'></div>
-            <div class='line-description'> ${lineDescription ? lineDescription : ''} </div>
+            <div id="canvas"></div>
         </div>
+
+        <slot name="options" class="options" on-slotchange="${(evt: Event) => this._onSlotChanged(evt)}" hidden> </slot>
         `;
+    }
+
+    /**
+     * Fired on slot change
+     * @param {Event} event
+     */
+    protected _onSlotChanged(event: Event): void {
+        console.log('_onSlotChanged:', Date.now());
+        const equationsElems: HTMLElement[] = [];
+        const slot: HTMLSlotElement = event.srcElement as HTMLSlotElement;
+        if (slot) {
+            slot.assignedNodes().forEach(
+                (el: HTMLElement): void => {
+                    equationsElems.push(el);
+                }
+            );
+        }
+
+        equationsElems.forEach((equationElem) => {
+            this.equations.push(equationElem.innerHTML);
+        });
+
+        this.requestRender();
     }
 }
 
