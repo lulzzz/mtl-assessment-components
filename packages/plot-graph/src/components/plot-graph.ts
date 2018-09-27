f0f14e90c5c460559fbdb343832b52f640686137
Christopher Morris <cmorris@tribalnova.com>
2018-09-27 17:55:27 -0400

    refactor per example

--- packages/plot-graph/src/components/plot-graph.ts
+++ packages/plot-graph/src/components/plot-graph.ts
@@ -31,6 +31,7 @@ export class PlotGraph extends ComponentBase<string> {
     // This is a mock
     private prepareValue(equation: string, x: number, y?: number) : number {
         const evalMe = equation.replace('x', x.toString());
+        console.log('prepareValue:', evalMe);
         return eval(evalMe);
     }
 
@@ -38,50 +39,16 @@ export class PlotGraph extends ComponentBase<string> {
         return (this.graphSize / 100) * val;
     }
 
-//This is the accessor function we talked about above
-    private getPoints(equation: string): string {
-        let results: string = '';
+    //This is the accessor function we talked about above
+    private getPoints(equation: string): Object[] {
+        let points: Object[] = [];
 
         for (let x = this.xmin; x <= this.xmax; x+=this.step) {
             const y = this.prepareValue(equation, x);
-            // results.push({"x": x, "y": y});
-            // console.log('x: ', x, ' y: ', y);
-            results+=`${this.scaleValToGraph(x).toString()},${this.scaleValToGraph(y).toString()}`;
+            points.push({ x: this.scaleValToGraph(x), y: this.scaleValToGraph(y) });
         }
 
-        return results;
-    }
-
-    public _didRender(): void {
-        console.log('_didRender:', Date.now());
-        
-        if (!this.renderedGraph && this.equations.length > 0) { //hacky badness
-            /*
-            let lineFunction = d3.svg.line()
-            .x(function(d: any) { return d.x; })
-            .y(function(d: any) { return d.y; })
-            .interpolate("linear");*/
-
-            this.renderedGraph = true;
-            this.svgContainer = d3.select(this.shadowRoot).select("#canvas").append("svg").attr("width", this.graphSize).attr("height", this.graphSize);
-
-            this.equations.forEach((equation) => {
-                const points = this.getPoints(equation);
-                /*
-                this.svgContainer.append("path")
-                                            .attr("d", lineFunction(points))
-                                            .attr("stroke", "blue")
-                                            .attr("stroke-width", 2)
-                                            .attr("fill", "none");
-
-                */
-                console.log('points: ', points);
-                this.svgContainer.append("polyline")
-                .attr("points", points)
-                .attr("stroke-width", 1)
-                .attr("stroke", 'red');                
-            });
-        }
+        return points;
     }
 
     protected _render({equations}: PlotGraph): TemplateResult {
@@ -95,6 +62,60 @@ export class PlotGraph extends ComponentBase<string> {
 
         <slot name="options" class="options" on-slotchange="${(evt: Event) => this._onSlotChanged(evt)}" hidden> </slot>
         `;
+    }
+
+    public _didRender(): void {
+        console.log('_didRender:', Date.now());
+        if (!this.renderedGraph && this.equations.length > 0) { //hacky badness
+            /*.interpolate("linear");*/
+            let numberPoints = 360;
+            this.renderedGraph = true;
+            this.svgContainer = d3.select(this.shadowRoot).select("#canvas").append("svg");
+
+            //TODO: typescript and per expression and dataset from expression
+
+
+            // 5. X scale will use the index of our data
+            var xScale = d3.scaleLinear()
+            .domain([0, numberPoints-1]) // input
+            .range([0, this.graphSize]); // output
+
+            // 6. Y scale will use the randomly generate number 
+            var yScale = d3.scaleLinear()
+            .domain([-1, 1]) // input 
+            .range([this.graphSize, 0]); // output           
+            
+            // 7. d3's line generator
+            var line = d3.line()
+                .x(function(d: any, i: any) { return xScale(i); }) // set the x values for the line generator
+                .y(function(d: any) { return yScale(d.y); }) // set the y values for the line generator 
+                .curve(d3.curveMonotoneX) // apply smoothing to the line            
+
+            // 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
+            var dataset = d3.range(numberPoints).map(function(d: any) { return {"y": Math.sin(d/30) } })
+
+            this.svgContainer.attr("width", this.graphSize)
+            .attr("height", this.graphSize)
+            .append("g")
+            .attr("transform", "translate(0,0)");
+
+            // 3. Call the x axis in a group tag
+            this.svgContainer.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + this.graphSize + ")")
+                .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
+
+            // 4. Call the y axis in a group tag
+            this.svgContainer.append("g")
+                .attr("class", "y axis")
+                .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
+
+            // 9. Append the path, bind the data, and call the line generator 
+            this.svgContainer.append("path")
+                .datum(dataset) // 10. Binds data to the line 
+                .attr("class", "line") // Assign a class for styling 
+                .attr("d", line); // 11. Calls the line generator 
+        }
     }
 
     /**
