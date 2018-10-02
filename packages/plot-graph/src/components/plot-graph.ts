4829bc26a92481052953b3b6fc26936a9b1f8868
Christopher Morris <cmorris@tribalnova.com>
2018-10-02 10:49:11 -0400

    update comments

--- packages/plot-graph/src/components/plot-graph.ts
+++ packages/plot-graph/src/components/plot-graph.ts
@@ -46,19 +46,17 @@ export class PlotGraph extends ComponentBase<string> {
 
             const numberPoints = (this.xmax - this.xmin / this.step);
             this.renderedGraph = true;
-            this.svgContainer = d3.select(this.shadowRoot).select("#canvas").append("svg")
+            this.svgContainer = d3.select(this.shadowRoot).select("#canvas").append("svg");
 
-            // 5. X scale will use the index of our data
             const xScale = d3.scaleLinear()
             .domain([this.xmin, this.xmax]) // input
             .range([0, this.graphSize]); // output
 
-            // 6. Y scale will use the randomly generate number 
             const yScale = d3.scaleLinear()
             .domain([this.ymin, this.ymax]) // input 
             .range([this.graphSize, 0]); // output     
 
-            // 7. d3's line generator
+            // Line generator
             const line = d3.line()
                 .x(function(d: any, i: any) { return xScale(i); }) // set the x values for the line generator
                 .y(function(d: any) { return yScale(d.y); }) // set the y values for the line generator 
@@ -70,19 +68,17 @@ export class PlotGraph extends ComponentBase<string> {
             .append("g")
             
             this.equations.forEach((equation) => {
-                // 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
-                // const prepareValue = this.prepareValue;
                 const dataset = d3.range(numberPoints).map(function(x: any) { return {"y": this.prepareValue(equation, x)}}.bind(this));
 
-                // 9. Append the path, bind the data, and call the line generator 
+                // Append the path, bind the data, and call the line generator 
                 this.svgContainer.append("path")
-                    .datum(dataset) // 10. Binds data to the line 
+                    .datum(dataset) // inds data to the line 
                     .attr("class", "line") // Assign a class for styling 
-                    .attr("d", line) // 11. Calls the line generator 
+                    .attr("d", line) // Calls the line generator 
                     .style("stroke", equation.getAttribute('color')) 
             });
-
-            // 3. Call the x axis in a group tag
+            
+            // Call the x axis in a group tag
             this.svgContainer.append("g")
                 .attr("class", "x-axis")
                 .attr("transform", "translate(0," + (this.graphSize - 25) + ")")
