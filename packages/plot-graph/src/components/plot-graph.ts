0dfe69163f8a5ef0eb68f2015d89a9d6cb834927
Dominique Rochefort <drochefort@tribalnova.com>
2018-10-02 13:14:07 -0400

    fix missing shadowRoot property and build

--- packages/plot-graph/src/components/plot-graph.ts
+++ packages/plot-graph/src/components/plot-graph.ts
@@ -1,8 +1,8 @@
-import { ComponentBase, html, TemplateResult, property } from "@hmh/component-base/dist/index";
-import * as d3 from "d3";
+import { ComponentBase, html, TemplateResult, property } from '@hmh/component-base/dist/index';
+import * as d3 from 'd3';
 
 // This is a mock
-function prepareValue(equation: HTMLElement, x: string) : number {
+function prepareValue(equation: HTMLElement, x: string): number {
     return eval(equation.innerHTML.replace('x', x));
 }
 
@@ -11,7 +11,6 @@ function prepareValue(equation: HTMLElement, x: string) : number {
  * @demo ./demo/index.html
  */
 export class PlotGraph extends ComponentBase<string> {
- 
     @property({ type: Number })
     public xmin: number = 0;
     @property({ type: Number })
@@ -25,10 +24,12 @@ export class PlotGraph extends ComponentBase<string> {
     @property({ type: Array })
     private equations: HTMLElement[] = [];
 
+    public shadowRoot: ShadowRoot;
+
     private graphSize: number = 500;
     private svgContainer: any = null;
     private renderedGraph: boolean = false;
-    
+
     protected render(): TemplateResult {
         return html`
         <link rel="stylesheet" type="text/css" href="/dist/css/plot-graph.css">
@@ -39,52 +40,65 @@ export class PlotGraph extends ComponentBase<string> {
 
     public updated(): void {
         if (!this.renderedGraph && this.equations.length > 0) {
-
-            const numberPoints = (this.xmax - this.xmin / this.step);
+            const numberPoints = this.xmax - this.xmin / this.step;
             this.renderedGraph = true;
-            this.svgContainer = d3.select(this.shadowRoot).select("#canvas").append("svg");
+            this.svgContainer = d3
+                .select(this.shadowRoot)
+                .select('#canvas')
+                .append('svg');
 
-            const xScale = d3.scaleLinear()
-            .domain([this.xmin, this.xmax]) // input
-            .range([0, this.graphSize]); // output
+            const xScale = d3
+                .scaleLinear()
+                .domain([this.xmin, this.xmax]) // input
+                .range([0, this.graphSize]); // output
 
-            const yScale = d3.scaleLinear()
-            .domain([this.ymin, this.ymax]) // input 
-            .range([this.graphSize, 0]); // output     
+            const yScale = d3
+                .scaleLinear()
+                .domain([this.ymin, this.ymax]) // input
+                .range([this.graphSize, 0]); // output
 
             // Line generator
-            const line = d3.line()
-                .x(function(d: any, i: any) { return xScale(i); }) // set the x values for the line generator
-                .y(function(d: any) { return yScale(d.y); }) // set the y values for the line generator 
-                .curve(d3.curveMonotoneX) // apply smoothing to the line
-     
-            this.svgContainer
-            .attr("width", this.graphSize)
-            .attr("height", this.graphSize)
-            .append("g")
-            
-            this.equations.forEach((equation) => {
-                const dataset = d3.range(numberPoints).map(function(x: any) { return {"y": prepareValue(equation, x) }});
+            const line = d3
+                .line()
+                .x(function(d: any, i: any) {
+                    return xScale(i);
+                }) // set the x values for the line generator
+                .y(function(d: any) {
+                    return yScale(d.y);
+                }) // set the y values for the line generator
+                .curve(d3.curveMonotoneX); // apply smoothing to the line
 
-                // Append the path, bind the data, and call the line generator 
-                this.svgContainer.append("path")
-                    .datum(dataset) // inds data to the line 
-                    .attr("class", "line") // Assign a class for styling 
-                    .attr("d", line) // Calls the line generator 
-                    .style("stroke", equation.getAttribute('color')) 
+            this.svgContainer
+                .attr('width', this.graphSize)
+                .attr('height', this.graphSize)
+                .append('g');
+
+            this.equations.forEach(equation => {
+                const dataset = d3.range(numberPoints).map(function(x: any) {
+                    return { y: prepareValue(equation, x) };
+                });
+
+                // Append the path, bind the data, and call the line generator
+                this.svgContainer
+                    .append('path')
+                    .datum(dataset) // inds data to the line
+                    .attr('class', 'line') // Assign a class for styling
+                    .attr('d', line) // Calls the line generator
+                    .style('stroke', equation.getAttribute('color'));
             });
-            
+
             // Call the x axis in a group tag
-            this.svgContainer.append("g")
-                .attr("class", "x-axis")
-                .attr("transform", "translate(0," + (this.graphSize - 25) + ")")
+            this.svgContainer
+                .append('g')
+                .attr('class', 'x-axis')
+                .attr('transform', 'translate(0,' + (this.graphSize - 25) + ')')
                 .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
-                
 
             // 4. Call the y axis in a group tag
-            this.svgContainer.append("g")
-                .attr("class", "y-axis")
-                .attr("transform", "translate(" + 25 + ",0)")
+            this.svgContainer
+                .append('g')
+                .attr('class', 'y-axis')
+                .attr('transform', 'translate(' + 25 + ',0)')
                 .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
         }
     }
@@ -106,8 +120,6 @@ export class PlotGraph extends ComponentBase<string> {
             this.equations = equations;
         }
     }
-
-
 }
 
-customElements.define("plot-graph", PlotGraph);
+customElements.define('plot-graph', PlotGraph);
