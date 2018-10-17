9d4aa64383d7fd840b2cd0fdaa1e7d8f184bea39
Christopher Morris <cmorris@tribalnova.com>
2018-10-17 16:57:16 -0400

    vis 2d with hardcoded data

--- packages/plot-graph-3d/src/components/plot-graph-3d.ts
+++ packages/plot-graph-3d/src/components/plot-graph-3d.ts
@@ -1,21 +1,4 @@
 import { html, TemplateResult, property, ComponentBase, CoordinateSystem } from '@hmh/component-base';
-import { line, curveMonotoneX } from 'd3-shape';
-import { range } from 'd3-array';
-import { axisBottom, axisLeft } from 'd3-axis';
-import { Line } from 'd3-shape';
-import { scaleLinear } from 'd3-scale';
-import { Graph2d, DataSet } from 'vis/dist/vis.js';
-
-// This is a mock
-function prepareValue(equation: HTMLElement, x: string): number {
-    return eval(equation.innerHTML.replace('x', x));
-}
-
-enum Direction {
-    X = 'X',
-    Y = 'Y',
-    Z = 'Z'
-}
 
 /**
  * `<plot-graph-3d>`
@@ -35,7 +18,8 @@ export class PlotGraph3D extends ComponentBase<any> {
     protected render(): TemplateResult {
         return html`
         <link rel="stylesheet" type="text/css" href="/css/plot-graph-3d.css">
-
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/vis/4.16.1/vis.css">
+        
         <div class="container">
             <div id="canvas"></div>
         </div>
@@ -62,39 +46,10 @@ export class PlotGraph3D extends ComponentBase<any> {
             );
 
             this.equationItems = equationItems;
-            this.drawGraph();     
+           // this.drawGraph();     
         }
     }
 
-    /**
-     * Return a d3.scale function
-     *
-     * @param  {Direction} axis X or Y
-     * @returns d3.ScaleLinear
-     */
-    private scale(axis: Direction, min: number, max: number, size: number): d3.ScaleLinear<number, number> {
-        const domain = [min, max];
-        const range = axis === Direction.X ? [0, size] : [size, 0];
-        return scaleLinear()
-            .domain(domain) // input
-            .range(range); // output
-    }
-
-    /**
-     * @param  {d3.ScaleLinear<number, number>} xScale
-     * @param  {d3.ScaleLinear<number, number>} yScale
-     * @returns Returns a D3 line defined by xScale and yScale
-     */
-    private drawLine(xScale: d3.ScaleLinear<number, number>, yScale: d3.ScaleLinear<number, number>): Line<any> {
-        return line()
-            .x(function(d: any, i: any) {
-                return xScale(i);
-            }) // set the x values for the line generator
-            .y(function(d: any) {
-                return yScale(d.y);
-            }) // set the y values for the line generator
-            .curve(curveMonotoneX); // apply smoothing to the line
-    }
 
     /**
      * A Coordinate System contains axis
@@ -123,18 +78,18 @@ export class PlotGraph3D extends ComponentBase<any> {
      * @returns void
      */
     private drawGraph(): void {
-        const aspect = 1;
         const canvas = this.shadowRoot.getElementById('canvas');
-        const width = canvas.clientWidth;
-        const height = Math.round(width / aspect);
 
+        while (canvas.firstChild) {
+            canvas.removeChild(canvas.firstChild);
+        }
 
-        // https://chartio.com/resources/tutorials/how-to-resize-an-svg-when-the-window-is-resized-in-d3-js/
-        canvas.setAttribute('width', '100%')
-        canvas.setAttribute('height', '100%');
+        var options = {
+            start: '2014-06-10',
+            end: '2014-06-18'
+        };
 
-        /*
-        var items = [
+        const items = [
             {x: '2014-06-11', y: 10},
             {x: '2014-06-12', y: 25},
             {x: '2014-06-13', y: 30},
@@ -142,51 +97,9 @@ export class PlotGraph3D extends ComponentBase<any> {
             {x: '2014-06-15', y: 15},
             {x: '2014-06-16', y: 30}
           ];
-        
-;
-          var options = {
-            start: '2014-06-10',
-            end: '2014-06-18'
-        };*/
 
-        // plot a line for each equation
-        this.equationItems.forEach(equation => {
-            const equationXmin = parseInt(equation.getAttribute('equation-xmin'));
-            const equationXmax = parseInt(equation.getAttribute('equation-xmax'));
-            const equationYmin = parseInt(equation.getAttribute('equation-ymin'));
-            const equationYmax = parseInt(equation.getAttribute('equation-ymax'));
-            const step = parseInt(equation.getAttribute('step'));
-            const xScale = this.scale(Direction.X, equationXmin, equationXmax, width);
-            const yScale = this.scale(Direction.Y, equationYmin, equationYmax, height);
-            const numberPoints = equationXmax - equationXmin / step;
 
-            // get Y for each X (apply equation)
-            const data = range(numberPoints).map(function(x: any) {
-                return { y: prepareValue(equation, x) };
-            });
-
-            const graph2d = new Graph2d(canvas, new DataSet(data));
-        });
-
-        const axisOffset: number = 20;
-        // trans X and Y of bottom axis
-        const translationX = 'translate(0,' + (height - axisOffset) + ')';
-        // trans X and Y of left axis
-        const translationY = 'translate(' + axisOffset + ',0)';
-
-        //draw the axes (assuming any have been added)
-        this.axes.forEach(axis => {
-            const isDirectionX = axis.getAttribute('direction').toLowerCase() === Direction.X.toString().toLowerCase();
-            const min = axis.getAttribute('min');
-            const max = axis.getAttribute('max');
-            const scale = this.scale(axis.direction, parseInt(min), parseInt(max), isDirectionX ? width : height);
-            
-            this.svgContainer
-                .append('g')
-                .attr('class', isDirectionX ? 'x-axis' : 'y-axis')
-                .attr('transform', isDirectionX ? translationX : translationY)
-                .call(isDirectionX ? axisBottom(scale) : axisLeft(scale));
-        });
+        const graph2d = new vis.Graph2d(canvas, new vis.DataSet(items), options);
     }
 }
 
