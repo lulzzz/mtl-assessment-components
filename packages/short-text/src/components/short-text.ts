da2912dfe5bb25be28bc99cf7cdfd19bc1c1506e
Christopher Morris <cmorris@tribalnova.com>
2018-10-24 12:48:39 -0400

    integrate quill

--- packages/short-text/src/components/short-text.ts
+++ packages/short-text/src/components/short-text.ts
@@ -1,4 +1,13 @@
 import { ComponentBase, html, TemplateResult } from '@hmh/component-base';
+/*
+import Quill from 'quill/core'
+import Toolbar from 'quill/modules/toolbar';
+import Snow from 'quill/themes/snow';
+
+import Bold from 'quill/formats/bold';
+import Italic from 'quill/formats/italic';
+import Header from 'quill/formats/header';
+*/
 
 /**
  * `<short-text>`
@@ -7,8 +16,40 @@ import { ComponentBase, html, TemplateResult } from '@hmh/component-base';
 export class ShortText extends ComponentBase<string> {
     public value: string;
 
+    /**
+     * @returns TemplateResult
+     */
     protected render(): TemplateResult {
-        return html``;
+        return html`
+        <link rel="stylesheet" type="text/css" href="/css/short-text.css">
+        <link rel="stylesheet" type="text/css" href="quill/dist/quill.core.css">
+        <link rel="stylesheet" type="text/css" href="quill/dist/quill.snow.css">
+        <span class='quill-container'> </span>
+        `;
+    }
+
+    /**
+     * Called after each render
+     *
+     * @returns void
+     */
+    protected updated(): void {
+        const options = {
+            debug: 'info',
+            modules: {
+              toolbar: [
+                [{ header: [1, 2, false] }],
+                ['bold', 'italic', 'underline'],
+                ['image', 'code-block']
+              ]
+            },
+            placeholder: 'placeholder!',
+            readOnly: false,
+            theme: 'snow'
+        };
+
+        const container = this.shadowRoot.querySelector('.quill-container');
+        new Quill(container, options);
     }
 }
 
